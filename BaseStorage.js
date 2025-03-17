15e49f92b7ed6c39ec186a220522e3670e204e08
Arina <Ukam.naletko@mail.ru>
2025-03-17 14:15:51 +0300

    -

--- BaseStorage.js
+++ BaseStorage.js
@@ -10,26 +10,26 @@
 // Methods:
 // - isEmpty() - returns a Boolean value (whether the storage is empty or not);
 // - toArray() - returns a new array consisting of stored elements.
-class BaseStorage{
-constructor(maxSize = 10){
-    if (typeof maxSize !== 'number' || maxSize <= 0) {
-        throw new Error('Invalid number!');
+class BaseStorage {
+    constructor(maxSize = 10) {
+        if (typeof maxSize !== 'number' || maxSize <= 0) {
+            throw new Error('Invalid number!');
+        }
+        this.storage = [];
+        this.maxSize = maxSize;
+
     }
-    this.storage = [];
-    this.maxSize = maxSize;
-  
-}
-isEmpty(){
-return (this.storage.length===0)
-}
-toArray(){
-return [...this.storage]
-}
+    isEmpty() {
+        return (this.storage.length === 0)
+    }
+    toArray() {
+        return [...this.storage]
+    }
 };
 
 try {
     const storage = new BaseStorage(5);
-    console.log(storage.isEmpty()); 
+    console.log(storage.isEmpty());
     console.log(storage.toArray());
 } catch (error) {
     console.error(error.message);
@@ -47,54 +47,51 @@ try {
 // - fromIterable(iterable) - returns a new Stack, the elements of which are the elements of the passed iterable entity.
 //  The maximum number of elements of such a stack must be equal to the length of this entity. If the entity is not iterable, generate an error.
 
-class Stack extends BaseStorage{
-   
-    push(elem){
-    if(this.maxSize <= this.storage.length)
-        throw new Error("Stack is full") 
-   let newstor=new Array(this.storage.length+1)
-   for(let index=0;index<this.storage.length;index++)
-   {
-   newstor[index]=this.storage[index]
-   }
-   newstor[this.storage.length]=elem
-   this.storage=newstor
+class Stack extends BaseStorage {
+
+    push(elem) {
+        if (this.maxSize <= this.storage.length)
+            throw new Error("Stack is full")
+        let newstor = new Array(this.storage.length + 1)
+        for (let index = 0; index < this.storage.length; index++) {
+            newstor[index] = this.storage[index]
+        }
+        newstor[this.storage.length] = elem
+        this.storage = newstor
     }
-    pop(){
+    pop() {
         if (this.isEmpty()) {
             throw new Error("Stack is empty");
         }
         let newstor = new Array(this.storage.length - 1);
-        for(let index=0;index<newstor.length;index++)
-            {
-            newstor[index]=this.storage[index]
-            }
-            const popped=this.storage[this.storage.length-1]
-            this.storage=newstor
-      return popped
+        for (let index = 0; index < newstor.length; index++) {
+            newstor[index] = this.storage[index]
+        }
+        const popped = this.storage[this.storage.length - 1]
+        this.storage = newstor
+        return popped
     }
-    peek(){
-        if(this.isEmpty())
-        {
+    peek() {
+        if (this.isEmpty()) {
             return null
         }
-        return this.storage[this.storage.length-1]
+        return this.storage[this.storage.length - 1]
     }
-    static fromIterable(iterable){
+    static fromIterable(iterable) {
         try {
-            for (const item of iterable) {} 
+            for (const item of iterable) { }
         } catch (e) {
             throw new Error('Not iterable');
         }
-        const newstack=new Stack()
+        const newstack = new Stack()
         for (const item of iterable) {
-            newstack.push(item); 
+            newstack.push(item);
         }
 
         return newstack;
     }
 }
-const st=new Stack()
+const st = new Stack()
 st.push(4)
 st.push(3)
 st.push(2)
@@ -123,54 +120,49 @@ console.log(st.peek())
 // Implement static public methods:
 // - fromIterable(iterable) - returns a new Queue, the elements of which are the elements of the passed iterable entity.
 //  The maximum number of elements of such a queue must be equal to the length of this entity. If the entity is not iterable, generate an error.
-class  Queue extends BaseStorage{
+class Queue extends BaseStorage {
 
-push(elem)
-{
-    if(this.maxSize <= this.storage.length)
-        throw new Error("Queue is full") 
-   let newstor=new Array(this.storage.length+1)
-   for(let index=0;index<this.storage.length;index++)
-   {
-   newstor[index]=this.storage[index]
-   }
-   newstor[this.storage.length]=elem
-   this.storage=newstor
-}
-shift()
-{
-    if (this.isEmpty()) {
-        throw new Error("Queue is empty");
-    }
-    let newstor = new Array(this.storage.length - 1);
-    for(let index=0;index<newstor.length;index++)
-        {
-        newstor[index]=this.storage[index+1]
+    push(elem) {
+        if (this.maxSize <= this.storage.length)
+            throw new Error("Queue is full")
+        let newstor = new Array(this.storage.length + 1)
+        for (let index = 0; index < this.storage.length; index++) {
+            newstor[index] = this.storage[index]
         }
-        const popped=this.storage[0]
-        this.storage=newstor
-  return popped
-}
-peek()
-{
-    if(this.isEmpty())return null
-    return this.storage[0]
-}
-static fromIterable(iterable){
-    try {
-        for (const item of iterable) {} 
-    } catch (e) {
-        throw new Error('Not iterable');
+        newstor[this.storage.length] = elem
+        this.storage = newstor
     }
-    const newqueue=new Queue()
-    for (const item of iterable) {
-        newqueue.push(item); 
+    shift() {
+        if (this.isEmpty()) {
+            throw new Error("Queue is empty");
+        }
+        let newstor = new Array(this.storage.length - 1);
+        for (let index = 0; index < newstor.length; index++) {
+            newstor[index] = this.storage[index + 1]
+        }
+        const popped = this.storage[0]
+        this.storage = newstor
+        return popped
     }
+    peek() {
+        if (this.isEmpty()) return null
+        return this.storage[0]
+    }
+    static fromIterable(iterable) {
+        try {
+            for (const item of iterable) { }
+        } catch (e) {
+            throw new Error('Not iterable');
+        }
+        const newqueue = new Queue()
+        for (const item of iterable) {
+            newqueue.push(item);
+        }
 
-    return newqueue;
+        return newqueue;
+    }
 }
-}
-const q=new Queue(5)
+const q = new Queue(5)
 q.push(4)
 q.push(1)
 q.push(2)
@@ -181,7 +173,7 @@ console.log(q.toArray())
 console.log(q.peek())
 const array = [1, 2, 3, 4, 5];
 const stackFromArray = Stack.fromIterable(array);
-console.log(stackFromArray.toArray()); 
+console.log(stackFromArray.toArray());
 
 const string = "hello";
 const stackFromString = Queue.fromIterable(string);
